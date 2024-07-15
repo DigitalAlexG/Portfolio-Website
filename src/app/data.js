19c6727588facbfb0881679ba2995729f101fbe0
Qiang Guo <qiangguoca@gmail.com>
2024-07-15 03:01:47 -0700

    the initial commit

---
+++ src/app/data.js
@@ -0,0 +1,109 @@
+// this comtains the data we are gonna for AboutMe component and slider component
+
+export const AboutData = [
+  {
+    count: 1,
+    title: "Front-end Developer",
+    img: "/first.svg",
+    array: [
+      "HTML / CSS3",
+      "javaScript (ES6)",
+      "ActionScript",
+      "ReactJS",
+      "NextJS",
+      "REST API",
+      "JSP",
+    ],
+  },
+  {
+    count: 2,
+    title: "Back-end Developer",
+    img: "/second.svg",
+    array: [
+      "JAVA",
+      "Python",
+      "C#",
+      "NodeJS",
+      "MongoDB / MySQL / Oracle",
+      "Spring Boot / Spring / Struts",
+      ".Net",
+      "MuleSoft",
+      "REST API",
+    ],
+  },
+  {
+    count: 3,
+    title: "Project Management",
+    img: "/third.svg",
+    array: [
+      "PMP Certification",
+      "Agile",
+      "Lead Team",
+      "Cross-functional collaboration",
+    ],
+  },
+  {
+    count: 4,
+    title: "Cloud",
+    img: "/fourth.svg",
+    array: ["AWS", "Azure", "Anypoint"],
+  },
+  {
+    count: 5,
+    title: "DevOps",
+    img: "/fifth.svg",
+    array: [
+      "Docker / CI CD",
+      "Apache / Nginx",
+      "CentOS / Ubuntu",
+      "Windows Server",
+      "Mail Servers / DNS Servers",
+    ],
+  },
+  {
+    count: 6,
+    title: "Tools",
+    img: "/sisxth.svg",
+    array: [
+      "VSCode",
+      "IntelliJ IDEA / Eclipse",
+      "WebStorm / PyCharm",
+      "Git / Github",
+      "Bash / Powershell",
+      "NPM / Yarn",
+      "Postman",
+    ],
+  },
+]
+
+// this is for the slider
+export const DataArray = [
+  {
+    title: "Portfolio Website",
+    name: "Portfolio Website",
+    des: "Here is my portfolio website, built using Reactjs and Nextjs. It works seamlessly on various devices and showcases my skills and projects. I have also included my contact information so that interested employers or recruiters can reach out to me at any time.",
+    des1: "This project was made using, Ractjs, Nextjs, NodeMailer  and many more!",
+    images: ["/portfolio.png", "/portfolio2.png"],
+  },
+  {
+    title: "Integration Project for the Three Gas Companies",
+    name: "Integration Project for the Three Gas Companies",
+    des: "The Integration Project for the Three Gas Companies aims to consolidate the similar business operations into a single platform. This platform achieves its functionalities by calling APIs and ensures data stability and security through AWS cloud storage.",
+    des1: "This project focuses primarily on backend development, including leading design, development, and testing efforts. The main technologies used are Java, Spring Boot, and AWS services.",
+    images: ["/3company.png"],
+  },
+  {
+    title: "7id Membership Integration Project",
+    name: "7id Membership Integration Project",
+    des: "The Seven & i Group includes various companies in retail, maternal and infant products, department stores, stationery, and delivery services. This system is designed to consolidate all member information from these companies into a single 7id membership, allowing one 7id membership to access services from all companies under The Seven & i Group.",
+    des1: "The frontend of this system is developed using technologies such as CSS, HTML, JavaScript, and JSP. The backend development utilizes JAVA, Struts, Spring, and Oracle technologies.",
+    images: ["/7iD.png"],
+  },
+  {
+    title: "Kid Coding Guide Website",
+    name: "Kid Coding Guide Website",
+    des: "This project is currently under development. As a volunteer teaching kids programming at the Okanagan Chinese Canadian Association (OCCA), I aim to create a coding guide website where children can learn programming online. Registered members can post questions for help when they encounter difficulties, submit unfinished assignments, and more.",
+    des1: "The frontend of this system uses React.js and Next.js, while the backend is developed with Node.js, Express.js, MongoDB, and AWS cloud services.",
+    images: ["/kids coding.png"],
+  },
+]
---
+++ src/app/page.js
@@ -0,0 +1,54 @@
+"use client"
+import HomeComponment from "./components/HomeComponent/HomeComponent"
+import { Hind } from "next/font/google"
+import MySlider from "./components/SliderCard/MySlider"
+import AboutMe from "./components/AboutMe/AboutMe"
+
+const hind = Hind({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+})
+
+export default function Home() {
+  return (
+    <main className="min-h-screen relative">
+      <HomeComponment />
+      <div
+        className="mt-0 pt-5 -mb-40"
+        id="portfolio"
+        style={{
+          backgroundImage:
+            "linear-gradient(-62deg, #EEF7FB 0 50%,  white 0% 100%)",
+          width: "100%",
+        }}
+      >
+        <div className="container m-auto">
+          <p
+            className="text-[300px]  text-[#F7FBFD] md:pl-[50px] px-5 max-w-[750px] w-[100%] overflow-hidden"
+            style={{ transform: "translate(0px,-20px)" }}
+          >
+            Portfolio
+          </p>
+          <div className="" style={{ transform: "translate(0px, -290px)" }}>
+            <p className="text-[#48AFDE]  md:pl-[80px] px-5 font-extrabold text-5xl">
+              Recent Works
+            </p>
+            <p
+              className={`max-w-2xl md:pl-[80px] px-5 font-[300] text-[16px] text-[#47626D] leading-8 mt-5 ${hind.className}`}
+            >
+              Here are a few of my work. As a full-stack web developer, I
+              constantly priorities 100% client satisfaction. I always enjoy
+              coding, each project is a new adventure for me. While working on
+              each new project, I attempt to learn new things. As a result, my
+              projects are always current and one-of-a-kind.
+            </p>
+          </div>
+        </div>
+        <div style={{ transform: "translate(0px,-230px)" }}>
+          <MySlider />
+        </div>
+      </div>
+      <AboutMe />
+    </main>
+  )
+}
