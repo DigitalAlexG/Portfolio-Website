eb223259cd3f7b06adc4c8127231baa777b39bf9
Qiang Guo <qiangguoca@gmail.com>
2024-07-16 16:34:29 -0700

    modify [contract me] style

--- src/app/page/contactme/page.js
+++ src/app/page/contactme/page.js
@@ -87,16 +87,18 @@ export default function Page() {
       <div className="flex flex-col items-center justify-center w-full pt-[80px] pb-[80px] mt-4">
         <div className='flex flex-col items-center justify-center bg-[url("/offices.jpg")] bg-cover bg-center w-full h-[400px]'>
           <div className="flex flex-col items-center justify-center  w-full h-full bg-[#223740]/70 backdrop-brightness-50">
-            {/* <div className="text-center bg-[#48AFDE] py-[5px] px-[10px] tracking-wide uppercase font-semibold text-[20px] text-white rounded-lg">
+            <div className="text-center bg-[#48AFDE] py-[10px] px-[10px] tracking-wide uppercase font-semibold text-[20px] text-white rounded-lg">
               <a href="#contact" className="cursor-pointer">
-                Contact Form
+                <h1 className="recoletaBold text-5xl text-[white] p-2 text-center tracking-wide">
+                  Contact Me
+                </h1>
               </a>
-            </div> */}
-            <div className="mt-[10px]">
+            </div>
+            {/* <div className="mt-[10px]">
               <h1 className="recoletaBold text-5xl text-[#48AFDE] p-2 text-center tracking-wide">
                 Contact Me
               </h1>
-            </div>
+            </div> */}
             <div className="flex justify-center text-center mt-[10px] md:w-[700px] ">
               <p className="text-white font-[300] text-cener text-xl">
                 Are you looking for a responsible, professional full-stack
